d50322b2babbb8bc39b4c0ce4dfa96391c79a6e9
Igor Suvorov <hi@isuvorov.com>
2023-09-27 14:20:24 +0300

    chore: update packages

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,3 +1,4 @@
+import { omitNull } from '@lskjs/algos/omitNull';
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
@@ -6,6 +7,20 @@ import { AppService } from './app.service';
 export class AppController {
   // eslint-disable-next-line no-empty-function
   constructor(private readonly appService: AppService) {}
+
+  @Get('/api/healthcheck')
+  async healthcheck() {
+    const nodejs = await new Promise((resolve) => {
+      const start = Date.now();
+      process.nextTick(() => {
+        resolve(Date.now() - start);
+      });
+    });
+    return omitNull({
+      status: 'ok',
+      nodejs,
+    });
+  }
 
   @Get('*')
   getHello(): string {
