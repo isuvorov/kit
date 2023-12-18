5b65dee621c6d591640de82d9f26a6c4f2d6ba86
Andrey Kondalov <andrey@kondalov.com>
2023-12-18 14:33:22 +0400

    test(signup): add test for too long password. SEN-35

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -1,6 +1,8 @@
 import { expect, test } from '@playwright/test';
 
+const email = `test+${Math.random().toString(36).substring(7)}@example.com`;
 const password = 'password';
+const passwordLong64 = '1234567890123456789012345678901234567890123456789012345678901234';
 
 test('Sign up with empty login', async ({ page }) => {
   await page.goto('https://kit.lskjs.ru/auth/login');
@@ -17,4 +19,17 @@ test('Sign up with empty login', async ({ page }) => {
   const errorMessage = await element.innerText();
   // // check the text
   expect(errorMessage).toBe('Email cannot be blank');
+});
+
+test('Sign up with too long password', async ({ page }) => {
+  await page.goto('https://kit.lskjs.ru/auth/signup');
+
+  await page.fill('[name="email"]', email);
+  await page.fill('[name="password"]', passwordLong64);
+  await page.click('[name="tos"]');
+  await page.click('[type="submit"]');
+
+  const [element] = await page.$$('.text-danger.form-text');
+  const errorMessage = await element.innerText();
+  expect(errorMessage).toBe('Password is too long. Maximum length allowed: 64 characters.');
 });
