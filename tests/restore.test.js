ca07abeb8754ca5ee0e8c73db8e919554a2fea13
Ivan Matskevich <faxtalk666@gmail.com>
2023-12-19 12:59:33 +0300

    test(restore): test incorrect email SEN-41

--- tests/restore.test.js
+++ tests/restore.test.js
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 const email = `notexistingemail${Math.random()}@gmail.com`;
+const incorrectEmailFormat = 'email@examplecom';
 
 test('Restore password with not existing email', async ({ page }) => {
   await page.goto('https://kit.lskjs.ru/auth/login');
@@ -13,4 +14,14 @@ test('Restore password with not existing email', async ({ page }) => {
   const errorMessage = await error.innerText();
   // check the text
   expect(errorMessage).toBe('Email not found');
+});
+
+test('Restore password with incorrect email format', async ({ page }) => {
+  await page.goto('https://kit.lskjs.ru/auth/login');
+
+  await page.getByText('Forgot Password?').click();
+  await page.fill('input[name="email"]', incorrectEmailFormat);
+  await page.click('button[type="submit"]');
+
+  await expect(page.getByText('Incorrect email format')).toBeVisible();
 });
