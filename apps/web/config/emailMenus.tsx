acfebd3b3f46effe5a6222ac8a0d53e740230b0a
Igor Suvorov <hi@isuvorov.com>
2024-02-20 18:29:31 +0300

    feat: emailMenus

---
+++ apps/web/config/emailMenus.tsx
@@ -0,0 +1,26 @@
+export const emailMenus = [
+  {
+    title: 'Welcome email',
+    href: '/admin/emails/welcome',
+  },
+  {
+    title: 'Reset password email',
+    href: '/admin/emails/reset',
+  },
+  {
+    title: 'Invite email',
+    href: '/admin/emails/invite',
+  },
+  {
+    title: 'Confirm email',
+    href: '/admin/emails/confirm',
+  },
+  {
+    title: 'Invoice email',
+    href: '/admin/emails/invoice',
+  },
+  {
+    title: 'Trial ending email',
+    href: '/admin/emails/trial',
+  },
+];
